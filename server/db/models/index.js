3d4ae5d4c37ac03e8349e576b78478b7fafb5c4d
Uma Huggins <umahuggins@gmail.com>
2018-10-29 16:11:14 -0400

    test commit

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,11 +1,15 @@
 const User = require('./user')
 
+
 /**
  * If we had any associations to make, this would be a great place to put them!
  * ex. if we had another model called BlogPost, we might say:
  *
  *    BlogPost.belongsTo(User)
+ * We have to add more models.
  */
+
+
 
 /**
  * We'll export all of our models here, so that any time a module needs a model,
