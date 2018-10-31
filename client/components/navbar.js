78b9295a349741ca48e754621ca1df3066280902
Nancy Wong <nancywong25@gmail.com>
2018-10-31 15:40:46 -0400

    Feature-Navigation Bar Filter By Category

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -7,11 +7,19 @@ import {logout} from '../store'
 const Navbar = ({handleClick, isLoggedIn}) => (
   <div>
     <nav>
-    <h1>Donut Jar</h1>
+      <h1>Donut Jar</h1> <br />
       {isLoggedIn ? (
         <div>
           {/* The navbar will show these links after you log in */}
-          <Link to="/home" className='home'>Home</Link>
+          <Link to="/round" className="filter">
+            Round Donuts
+          </Link>
+          <Link to="/holey" className="filter">
+            Holey Donuts
+          </Link>
+          <Link to="/home" className="home">
+            Home
+          </Link>
           <a href="#" onClick={handleClick}>
             Logout
           </a>
@@ -19,8 +27,18 @@ const Navbar = ({handleClick, isLoggedIn}) => (
       ) : (
         <div>
           {/* The navbar will show these links before you log in */}
-          <Link to="/login" className='signin'>Login</Link>
-          <Link to="/signup" className='signin'>Sign Up</Link>
+          <Link to="/round" className="filter">
+            Round Donuts
+          </Link>
+          <Link to="/holey" className="filter">
+            Holey Donuts
+          </Link>
+          <Link to="/login" className="signin">
+            Login
+          </Link>
+          <Link to="/signup" className="signin">
+            Sign Up
+          </Link>
         </div>
       )}
     </nav>
