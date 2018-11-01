fffb1d7af0dc2e9340848529a99ad0881dabe189
rumroses12 <grace@daher.net>
2018-10-31 20:36:18 -0400

    fix(store/product.js, components/SingleProduct)duplicate products are added together in store

--- client/components/CartView.js
+++ client/components/CartView.js
@@ -3,30 +3,9 @@ import {Link, withRouter} from 'react-router-dom'
 import {connect} from 'react-redux'
 import {StatelessSingleProduct} from './index'
 
-const dummy = {
-  id: 1,
-  title: 'Birthday Batter',
-  description: 'Doughtnut cake batter and sprinkles',
-  price: '3',
-  inventory: 5,
-  imageUrl: '/Birthday-Batter.png',
-  category: 'Round',
-  createdAt: '2018-10-30T20:21:34.433Z',
-  updatedAt: '2018-10-30T20:21:34.433Z',
-  review: {
-    id: 2,
-    content: 'Delicious donuts!',
-    rating: 5,
-    createdAt: '2018-10-30T20:21:34.476Z',
-    updatedAt: '2018-10-30T20:21:34.476Z',
-    userId: 2,
-    productId: 1
-  }
-}
-
 class CartView extends React.Component {
   render() {
-    return (
+    return this.props.cart[0] ? (
       <table>
         <thead>
           <tr>
@@ -48,6 +27,8 @@ class CartView extends React.Component {
           )
         })}
       </table>
+    ) : (
+      'No Items Yet!'
     )
   }
 }
