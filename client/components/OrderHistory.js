cc61317647b04b08707c703f5645da3a87787b31
rumroses12 <grace@daher.net>
2018-11-05 08:06:53 -0500

    feature(OrderHistory component, front and back end routes, user reducer and store)added view for OrderHistory

--- client/components/OrderHistory.js
+++ client/components/OrderHistory.js
@@ -1,7 +1,49 @@
 import React from 'react'
+import {StatelessOrderView} from './StatelessOrderView'
 
-const OrderHistory = (props) => {
-    return(
-        
-    )
+export const OrderHistory = props => {
+  //product.imageUrl
+  //product.id
+  //product.title
+  //number
+  const historyObj = {}
+  console.log('hello;', props)
+
+  props.orderHistory.map(elem => {
+    const date = elem.createdAt
+    elem.products.forEach(elem2 => {
+      const product = {
+        number: elem2.orderProducts.quantity,
+        status: elem.orderStatus,
+        product: {
+          title: elem2.title,
+          id: elem2.id,
+          imageUrl: elem2.imageUrl
+        }
+      }
+      if (!historyObj[date]) {
+        historyObj[date] = [product]
+      } else {
+        historyObj[date].push(product)
+      }
+      console.log(elem2)
+    })
+    console.log('historyObj', historyObj, historyObj[date])
+    historyObj[date]
+  })
+
+  return (
+    <div>
+      {Object.keys(historyObj).map((key, idx) => {
+        const status = historyObj[key][0].status
+        console.log(status)
+        return (
+          <div key={idx}>
+            {`Order Created: ${key}   Status: ${status}`}
+            <StatelessOrderView cart={historyObj[key]} />
+          </div>
+        )
+      })}
+    </div>
+  )
 }
--- client/store/user.js
+++ client/store/user.js
@@ -7,15 +7,16 @@ import history from '../history'
 const REMOVE_USER = 'REMOVE_USER'
 const DELETE_USER = 'DELETE_USER'
 const GET_USER = 'GET_USER'
-const GET_ALL_USERS= 'GET_ALL_USERS'
-const PROMOTE_USER= 'PROMOTE_USER'
-
+const GET_ALL_USERS = 'GET_ALL_USERS'
+const PROMOTE_USER = 'PROMOTE_USER'
+const GET_ORDER_HISTORY = 'GET_ORDER_HISTORY'
 /**
  * INITIAL STATE
  */
 const defaultUser = {
   user: {},
-  users:[]
+  users: [],
+  orderHistory: []
 }
 
 /**
@@ -23,12 +24,17 @@ const defaultUser = {
  */
 const getUser = user => ({type: GET_USER, user})
 const removeUser = () => ({type: REMOVE_USER})
-const deleteUser = (user) => ({type: DELETE_USER, user})
-const upgradeUser = (user) => ({type: PROMOTE_USER, user})
+const deleteUser = user => ({type: DELETE_USER, user})
+const upgradeUser = user => ({type: PROMOTE_USER, user})
 
 const getUsers = users => ({
   type: GET_ALL_USERS,
   users
+})
+
+const getOrderHistory = orders => ({
+  type: GET_ORDER_HISTORY,
+  orders
 })
 
 /**
@@ -82,18 +88,27 @@ export const logout = () => async dispatch => {
 }
 
 export const removingUser = id => dispatch => {
-  axios.delete(`/api/users/${id}`)
-      .then(() => dispatch(deleteUser(id)))
-      .catch(err => console.error(`Removing user: ${id} unsuccessful`, err));
-};
+  axios
+    .delete(`/api/users/${id}`)
+    .then(() => dispatch(deleteUser(id)))
+    .catch(err => console.error(`Removing user: ${id} unsuccessful`, err))
+}
 
 export const upgradingUser = id => dispatch => {
-  axios.put(`/api/users/${id}`)
-      .then(() => dispatch(upgradeUser(id)))
-      .catch(err => console.error(`Promoting user: ${id} unsuccessful`, err));
-};
+  axios
+    .put(`/api/users/${id}`)
+    .then(() => dispatch(upgradeUser(id)))
+    .catch(err => console.error(`Promoting user: ${id} unsuccessful`, err))
+}
 
-
+export const fetchOrderHistory = () => async dispatch => {
+  try {
+    const {data} = await axios.get('/api/users/orders')
+    dispatch(getOrderHistory(data))
+  } catch (error) {
+    console.error('fetchOrderHistory did not work', error)
+  }
+}
 /**
  * REDUCER
  */
@@ -108,10 +123,18 @@ export default function(state = defaultUser, action) {
       const us = newList.find(user => user.id === action.user)
       us.adminStatus = true
       return {...state, users: newList}
-   case DELETE_USER:
-      return {...state, users: [...state.users.filter(user => user.id !== action.user)]}
+    case DELETE_USER:
+      return {
+        ...state,
+        users: [...state.users.filter(user => user.id !== action.user)]
+      }
     case REMOVE_USER:
       return defaultUser
+    case GET_ORDER_HISTORY:
+      return {
+        ...state,
+        orderHistory: action.orders
+      }
     default:
       return state
   }
