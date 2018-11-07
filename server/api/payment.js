5e85c776fc186b63a8ae608ee02d09c0203282fa
rumroses12 <grace@daher.net>
2018-11-06 21:38:26 -0500

    fix(adding try/catch)

--- server/api/payment.js
+++ server/api/payment.js
@@ -1,23 +1,35 @@
 const router = require('express').Router()
-const stripe = require('../constants/stripe');
-
+const stripe = require('../constants/stripe')
 
 const postStripeCharge = res => (stripeErr, stripeRes) => {
-  if (stripeErr) {
-    res.status(500).send({ error: stripeErr });
-  } else {
-    res.status(200).send({ success: stripeRes });
+  try {
+    if (stripeErr) {
+      res.status(500).send({error: stripeErr})
+    } else {
+      res.status(200).send({success: stripeRes})
+    }
+  } catch (error) {
+    next(error)
   }
 }
 
 router.get('/', (req, res) => {
-  res.send({ message: 'Hello Stripe checkout server!', timestamp: new Date().toISOString() })
-});
-
+  try {
+    res.send({
+      message: 'Hello Stripe checkout server!',
+      timestamp: new Date().toISOString()
+    })
+  } catch (error) {
+    next(error)
+  }
+})
 
 router.post('/', (req, res) => {
-  stripe.charges.create(req.body, postStripeCharge(res));
-});
-
+  try {
+    stripe.charges.create(req.body, postStripeCharge(res))
+  } catch (error) {
+    next(error)
+  }
+})
 
 module.exports = router
